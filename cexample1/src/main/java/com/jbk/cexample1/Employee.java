3290a6f28260e220061e8a216725573e8bfcd2d1
PHOENIX CAFE <PHOENIX CAFE@PHOENIXCAFE-PC>
2019-12-17 18:11:04 +0530

    changes

---
+++ cexample1/src/main/java/com/jbk/cexample1/Employee.java
@@ -0,0 +1,29 @@
+package com.jbk.cexample1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	private String employeeAddress;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeAddress() {
+		return employeeAddress;
+	}
+	public void setEmployeeAddress(String employeeAddress) {
+		this.employeeAddress = employeeAddress;
+	}
+
+}
